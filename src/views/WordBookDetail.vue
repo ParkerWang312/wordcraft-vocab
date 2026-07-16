d146c40861af69e4693b3065270908f70588ba74
ParkerWang312 <wanglihua_312@163.com>
2026-07-16 10:00:42 +0800

    fix: 默写按钮文字改回「练习完再默写」

--- src/views/WordBookDetail.vue
+++ src/views/WordBookDetail.vue
@@ -56,7 +56,7 @@
         :disabled="!dictationAvailable"
         @click="goDictation"
       >
-        {{ dictationAvailable ? '开始默写' : '练完再默写' }}
+        {{ dictationAvailable ? '开始默写' : '练习完再默写' }}
       </van-button>
     </div>
 
@@ -369,7 +369,7 @@ function showBatchDialog() {
 
 .action-row-double .van-button {
   flex: 1;
-  font-size: 15px;
+  font-size: 14px;
 }
 
 .word-list {
